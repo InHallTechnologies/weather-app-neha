18a93bb35e8de81c70acdd572b559f20281aa9d6
Rishabh Verma <inhall.updatebackup@gmail.com>
2022-11-07 16:44:22 +0530

    trial

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+ import React from 'react';
+ import { NavigationContainer } from '@react-navigation/native';
+ import { createNativeStackNavigator } from '@react-navigation/native-stack';
+ import Home from './components/Home';
+import CountryInfo from './components/CountryInfo';
+import WeatherInfo from './components/WeatherInfo';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+ 
+ 
+ 
+ const Stack = createNativeStackNavigator();
+ 
+ const App = () => {
+   return (
+     <NavigationContainer>
+       <Stack.Navigator initialRouteName='Home'>
+         <Stack.Screen name="Home" component={Home} />
+         <Stack.Screen name="CountryInfo" component={CountryInfo} />
+         <Stack.Screen name="WeatherInfo" component={WeatherInfo} />
+       </Stack.Navigator>
+     </NavigationContainer>
+   );
+ };
+ 
+ 
+ 
+ export default App;
+ 
